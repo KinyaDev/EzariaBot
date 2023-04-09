42fdc28865ca2668504056be980cd8bf109b001d
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2023-04-09 12:54:54 +0200

    new commands and cleaner code and improved see command

--- commands/see.js
+++ commands/see.js
@@ -1,52 +1,94 @@
 const { SlashCommandBuilder, CommandInteraction } = require("discord.js");
-const hrpChannels = [
-  "1042757971895128105",
-  "1049303180317569105",
-  "1049037836743094282",
-  "1049294575157653564",
-  "1049295564690432050",
-  "1049023959053639782",
-];
+
+const linked_places = require("./linked_places.json");
+const RPChannels = Object.keys(linked_places);
+
+/**
+ * @param {CommandInteraction} interaction
+ * @param {string} id
+ */
+async function isLinked(interaction, id) {
+  let ret = false;
+  if (
+    interaction.channel.isTextBased() &&
+    RPChannels.includes(interaction.channel.parent.id)
+  ) {
+    /**
+     * @type {string[]}
+     */
+    let linked = linked_places[interaction.channel.parent.id];
+    let channels = (await interaction.guild.channels.fetch())
+      .filter((ch) => RPChannels.includes(ch.id))
+      .filter((c) => !c.parent)
+      .filter((c) => linked.includes(c.id));
+
+    console.log(channels);
+
+    if (channels.find((c) => c.id === id)) ret = true;
+  }
+
+  return ret;
+}
+
+/**
+ * @param {CommandInteraction} interaction
+ * @param {string} id
+ */
+async function travel(interaction, id) {
+  function ov(bool, ch) {
+    ch.permissionOverwrites.create(interaction.member, { ViewChannel: bool });
+  }
+
+  function children(id) {
+    return interaction.guild.channels.cache.filter((c) => c.parentId === id);
+  }
+
+  let channels = (await interaction.guild.channels.fetch())
+    .filter((h) => RPChannels.includes(h.id))
+    .filter((h) => !h.parent);
+
+  let linked = await isLinked(interaction, id);
+  if (linked) {
+    let aimed_channel = channels.find((c) => c.id === id);
+    if (aimed_channel) {
+      ov(true, aimed_channel);
+      for (let [child_id, child] of children(aimed_channel.id)) {
+        if (child.name.includes("❓")) {
+          ov(false, child);
+        } else {
+          ov(true, child);
+        }
+      }
+
+      let others = channels.filter((c) => c.id !== id);
+
+      others.forEach((ch) => {
+        ov(false, ch);
+        for (let [child_id, child] of children(ch.id)) {
+          ov(false, child);
+        }
+      });
+
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
 
 module.exports.data = new SlashCommandBuilder()
   .setName("see")
   .setDescription("Get access to a category using its name")
   .addStringOption((option) =>
-    option
-      .setDescription("Category")
-      .setName("category")
-      .setRequired(true)
-      .setChoices(
-        { value: "Plaines", name: "Plaines" },
-        { value: "Forêt", name: "Forêt" },
-        { value: "Ville", name: "Ville" },
-        { value: "Hôtel", name: "Hôtel" },
-        { value: "Montagnes", name: "Montagnes" },
-        { value: "Îles Volantes", name: "Îles Volantes" },
-        { value: "Ville Volante", name: "Ville Volante" },
-        { value: "Jungle", name: "Jungle" },
-        { value: "Désert", name: "Désert" },
-        { value: "Zone Gelée", name: "Zone Gelée" }
-      )
-  )
-  .addStringOption((option) =>
-    option
-      .setDescription("Character Name")
-      .setName("charname")
-      .setRequired(false)
+    option.setDescription("Category").setName("category").setRequired(true)
   );
 
 /**
  * @param {CommandInteraction} interaction
  */
 module.exports.run = async (interaction) => {
-  function ov(bool, ch) {
-    ch.permissionOverwrites.create(interaction.member, { ViewChannel: bool });
-  }
-  function children(id) {
-    return interaction.guild.channels.cache.filter((c) => c.parentId === id);
-  }
-
   let role = await interaction.guild.roles.fetch(process.env.VERIFIED_ROLE_ID);
 
   if (!role.members.get(interaction.user.id))
@@ -55,29 +97,21 @@ module.exports.run = async (interaction) => {
       flags: ["Ephemeral"],
     });
 
-  let channels = (await interaction.guild.channels.fetch())
-    .filter((h) => !hrpChannels.includes(h.id))
-    .filter((h) => !h.parent);
+  let name = interaction.options.get("category").value;
+  let ch = interaction.guild.channels.cache.find(
+    (c) => c.name.toLowerCase() === name.toLowerCase()
+  );
 
-  let name = interaction.options.get("category", true).value;
-  let ch = channels.find((c) => c.name.toLowerCase() === name.toLowerCase());
-
-  if (!ch) return;
-  let charname = interaction.options.get("charname");
-  let notIt = channels.filter((c) => c.id !== ch.id);
-  const usn = charname ? charname : interaction.member.user;
-
-  interaction.channel.send(`${usn} voyages vers <#${ch.id}>`);
-
-  ov(true, ch);
-  for (let [child_id, child] of children(ch.id)) {
-    ov(true, child);
+  let good = await travel(interaction, ch.id);
+  if (good) {
+    interaction.reply({
+      content: `Vous voilà à <#${ch.id}>`,
+      flags: ["Ephemeral"],
+    });
+  } else {
+    interaction.reply({
+      content: `Une erreur est survenue! <#${ch.id}>`,
+      flags: ["Ephemeral"],
+    });
   }
-
-  notIt.forEach((ch) => {
-    ov(false, ch);
-    for (let [child_id, child] of children(ch.id)) {
-      ov(false, child);
-    }
-  });
 };
---
+++ commands/seek.js
@@ -0,0 +1,102 @@
+const { SlashCommandBuilder, CommandInteraction } = require("discord.js");
+require("dotenv").config();
+const linked_places = require("./linked_places.json");
+const RPChannels = Object.keys(linked_places);
+
+module.exports.data = new SlashCommandBuilder()
+  .setName("seek")
+  .setDescription("trouver un donjon dans le lieu.");
+
+/**
+ * @param {CommandInteraction} interaction
+ */
+module.exports.run = async (interaction) => {
+  function children(id) {
+    return interaction.guild.channels.cache.filter((c) => c.parentId === id);
+  }
+
+  let d = Math.random();
+
+  let channelId = interaction.channel.id;
+  let parentId = interaction.channel.parentId;
+  let logchannel = await interaction.guild.channels.fetch(
+    process.env.LOG_CHANNEL_ID
+  );
+
+  if (
+    channelId === "1049028357565710408" ||
+    channelId === "1049293722203979876"
+  ) {
+    if (d < 0.5) {
+      for (let [child_id, child] of children(parentId)) {
+        if (child.name.includes("❓")) {
+          child.permissionOverwrites.create(interaction.member, {
+            ViewChannel: true,
+          });
+
+          logchannel.send(
+            `${interaction.member} a trouvé un donjon ! <#${child.id}>`
+          );
+
+          interaction.reply({
+            content: `Vous avez trouvé un donjon ! <#${child.id}>`,
+            flags: ["Ephemeral"],
+          });
+        }
+      }
+    } else
+      interaction.reply({
+        content: "Vous avez cherché un donjon, mais rien trouvé...",
+        flags: ["Ephemeral"],
+      });
+  } else if (
+    parentId === "1049023999671279648" ||
+    channelId === "1049294467498250310"
+  ) {
+    if (d < 0.4) {
+      for (let [child_id, child] of children(parentId)) {
+        if (child.name.includes("❓")) {
+          child.permissionOverwrites.create(interaction.member, {
+            ViewChannel: true,
+          });
+
+          logchannel.send(
+            `${interaction.member} a trouvé un donjon ! <#${child.id}>`
+          );
+
+          interaction.reply({
+            content: `Vous avez trouvé un donjon ! <#${child.id}>`,
+            flags: ["Ephemeral"],
+          });
+        }
+      }
+    } else
+      interaction.reply({
+        content: "Vous avez cherché un donjon, mais rien trouvé...",
+        flags: ["Ephemeral"],
+      });
+  } else if (!(parentId === "1049027478611566653")) {
+    if (d < 0.15) {
+      for (let [child_id, child] of children(parentId)) {
+        if (child.name.includes("❓")) {
+          child.permissionOverwrites.create(interaction.member, {
+            ViewChannel: true,
+          });
+
+          logchannel.send(
+            `${interaction.member} a trouvé un donjon ! <#${child.id}>`
+          );
+
+          interaction.reply({
+            content: `Vous avez trouvé un donjon ! <#${child.id}>`,
+            flags: ["Ephemeral"],
+          });
+        }
+      }
+    } else
+      interaction.reply({
+        content: "Vous avez cherché un donjon, mais rien trouvé...",
+        flags: ["Ephemeral"],
+      });
+  }
+};
