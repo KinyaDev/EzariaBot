28e3b909de84cda0ccf9df99fb225e61116a55da
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2023-04-12 15:02:30 +0200

    update

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,30 +9,24 @@ const { welcomeId } = require("../ids");
 module.exports = async (member) => {
   let welcomeChannel = await member.guild.channels.fetch(welcomeId);
   if (welcomeChannel && welcomeChannel.type === ChannelType.GuildText) {
-    welcomeChannel
-      .send({
-        embeds: [
-          {
-            title: `Bienvenue ${member.user.username} !`,
-            description: `Merci d'être venu! Nous sommes maintenant ${
-              member.guild.memberCount -
-              member.guild.members.cache.filter((f) => f.user.bot).size
-            }`,
-            thumbnail: { url: member.user.avatarURL() || member.avatarURL() },
-            author: {
-              name: member.user.username,
-              icon_url: member.user.avatarURL() || member.avatarURL(),
-            },
-
-            footer: { text: `0 célèbrent votre venue!` },
+    welcomeChannel.send({
+      embeds: [
+        {
+          title: `Bienvenue ${member.user.username} !`,
+          description: `Merci d'être venu! Nous sommes maintenant ${
+            member.guild.memberCount -
+            member.guild.members.cache.filter((f) => f.user.bot).size
+          }`,
+          thumbnail: { url: member.user.avatarURL() || member.avatarURL() },
+          author: {
+            name: member.user.username,
+            icon_url: member.user.avatarURL() || member.avatarURL(),
           },
-        ],
-        components: [
-          new ActionRowBuilder().addComponents(celebrateButton).toJSON(),
-        ],
-      })
-      .then((msg) => {
-        msg.metadata.push({ name: "celebrating", value: "" });
-      });
+        },
+      ],
+      components: [
+        new ActionRowBuilder().addComponents(celebrateButton).toJSON(),
+      ],
+    });
   }
 };
--- lib.js
+++
@@ -1,7 +0,0 @@
-const { ButtonBuilder, ButtonStyle } = require("discord.js");
-
-module.exports.celebrateButton = new ButtonBuilder()
-  .setCustomId("celebratebtn")
-  .setEmoji("🎉")
-  .setLabel("Célébrer")
-  .setStyle(ButtonStyle.Primary);
